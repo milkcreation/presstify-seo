93f4fc3530aebe477a23d12d9c75e13698167257
Jordy Manner <jordy.manner@milkcreation.fr>
2018-10-20 07:21:13 +0000

    seo -- tag 2.0.5 -- adaptation evol tiFy

---
+++ Wp/Manager.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace tiFy\Plugins\Seo\Wp;
+
+use tiFy\Metabox\Metabox;
+use tiFy\Kernel\Parameters\AbstractParametersBag;
+use tiFy\Plugins\Seo\Metabox\PostMetatag\PostMetatag;
+use tiFy\Plugins\Seo\SeoResolverTrait;
+
+class Manager extends AbstractParametersBag
+{
+    use SeoResolverTrait;
+
+    /**
+     * CONSTRUCTEUR.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        add_action(
+            'init',
+            function () {
+                $attrs = config('seo.wp', []);
+                $this->parse($attrs);
+
+                /** @var Metabox $metabox */
+                $metabox = resolve(Metabox::class);
+
+                $post_types = $this->has('post_type')
+                    ? $this->get('post_type', [])
+                    : array_keys(get_post_types());
+
+                $post_types = array_diff(
+                    $post_types, [
+                        'attachment',
+                        'custom_css',
+                        'customize_changeset',
+                        'nav_menu_item',
+                        'oembed_cache',
+                        'revision',
+                        'user_request'
+                    ]
+                );
+
+                foreach ($post_types as $post_type) :
+                    /** @var WP_Post_Type $wp_post_type */
+                    if (!$wp_post_type = get_post_type_object($post_type)) :
+                        continue;
+                    elseif (!$wp_post_type->public) :
+                        continue;
+                    elseif (!$wp_post_type->publicly_queryable && ($post_type !== 'page')) :
+                        continue;
+                    endif;
+
+                    $metabox->add(
+                        "{$post_type}@post_type",
+                        [
+                            'name'    => "SeoPostMetatag-{$post_type}",
+                            'content' => PostMetatag::class
+                        ]
+                    );
+                endforeach;
+            },
+            999999
+        );
+    }
+}
