7a1476200ac08376465d48d1ebada3fdbc1eed64
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2018-10-20 07:21:13 +0000

    seo -- tag 2.0.5 -- adaptation evol tiFy

---
+++ seo/tags/2.0.5/Seo.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * @name Seo
+ * @desc Gestion des données de référencement.
+ * @author Jordy Manner <jordy@milkcreation.fr>
+ * @package presstify-plugins/seo
+ * @namespace \tiFy\Plugins\Seo
+ * @version 2.0.5
+ */
+
+namespace tiFy\Plugins\Seo;
+
+use tiFy\Metabox\Metabox;
+use tiFy\Plugins\Seo\SeoResolverTrait;
+
+final class Seo
+{
+    use SeoResolverTrait;
+
+    /**
+     * CONSTRUCTEUR.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        add_action('init', function () {
+            /** @var Metabox $metabox */
+            $metabox = resolve(Metabox::class);
+
+            $metabox
+                ->add(
+                    'tify_options@options',
+                    [
+                        'name'  => 'SeoOptions',
+                        'title' => __('Référencement', 'tify'),
+                    ]
+                );
+            },
+            999999
+        );
+    }
+}
